ba7476ee49569627d24acc16bc06dccb7c18ad0c
Ochieng Paul <ochiengpaul442@gmail.com>
2025-01-20 12:11:49 +0300

    url updates

--- website/src/hooks/useInvestSubmission.ts
+++ website/src/hooks/useInvestSubmission.ts
@@ -2,5 +2,5 @@ import { postInvestment } from "@/app/server/api";
 import useSWRMutation from "swr/mutation";
 
 export function useInvestSubmission() {
-  return useSWRMutation("/invest/", postInvestment);
+  return useSWRMutation("/invest", postInvestment);
 }
