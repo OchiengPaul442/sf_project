deefae440a4bf12df81016205dfdf9f31177b3ca
Ochieng Paul <ochiengpaul442@gmail.com>
2025-01-24 00:48:17 +0300

    up

--- website/src/components/NextButton.tsx
+++ website/src/components/NextButton.tsx
@@ -11,22 +11,22 @@ const NextButton: React.FC<NextButtonProps> = ({ onClick, isVisible }) => {
     <AnimatePresence>
       {isVisible && (
         <motion.button
-          className="hidden fixed lg:block bottom-4 right-4 sm:bottom-6 sm:right-6 md:bottom-8 md:right-8 bg-white text-black border border-black rounded-full p-2 sm:p-3 shadow-lg z-50 transition-colors hover:bg-black hover:text-white"
+          className="fixed bottom-5 right-5 sm:bottom-7 sm:right-7 md:bottom-8 md:right-8 bg-white text-black border border-black rounded-full p-2 sm:p-3 shadow-lg z-50 transition-colors hover:bg-black hover:text-white"
           onClick={onClick}
-          initial={{ opacity: 0, y: 20 }}
+          initial={{ opacity: 0, y: 10 }}
           animate={{
             opacity: 1,
             y: 0,
             transition: {
               y: {
-                duration: 0.6,
+                duration: 0.8,
                 repeat: Number.POSITIVE_INFINITY,
                 repeatType: "reverse",
                 ease: "easeInOut",
               },
             },
           }}
-          exit={{ opacity: 0, y: 20 }}
+          exit={{ opacity: 0, y: 10 }}
           whileHover={{
             scale: 1.1,
             transition: { duration: 0.2 },
@@ -35,7 +35,7 @@ const NextButton: React.FC<NextButtonProps> = ({ onClick, isVisible }) => {
         >
           <motion.div
             animate={{
-              y: [0, -4, 0],
+              y: [0, -3, 0],
               transition: {
                 y: {
                   duration: 0.6,
